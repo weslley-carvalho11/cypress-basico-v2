34625616971dca914c75941accfef4add189d22c
Weslley90 <weslleycarvalho344@gmail.com>
2024-07-17 20:22:29 -0300

    breack the app on purpose

--- cypress/integration/CAC-TAT-spec.js
+++ cypress/integration/CAC-TAT-spec.js
@@ -168,7 +168,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
             .should('not.have.value')
             .selectFile('./cypress/fixtures/example.json', { action: 'drag-drop' })
             .should(function ($input) {
-                expect($input[0].files[0].name).to.equal('example.json')
+                expect($input[1].files[0].name).to.equal('example.json')
             })
     })
 
